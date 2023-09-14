6d21299a19dfc5eb0c2f4d2d6bc058b38fdd83e0
babydong <2216049628@qq.com>
2023-09-14 19:44:17 +0800

    Update: UI load

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+# 单PyQt打包
+# nuitka --mingw64 --standalone --show-progress --show-memory --enable-plugin=pyqt6 --windows-disable-console --output-dir=nuitka_out app.py
 from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.uic.load_ui import loadUi
 from PyQt6.QtCore import Qt
 
 from base.draggable import DraggableWindow
+from base.main_ui import Ui_Form
 
 
-class main_window(DraggableWindow):
+class main_window(DraggableWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        loadUi("base/main.ui", self)
+        self.setupUi(self)
         self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
 
